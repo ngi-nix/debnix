e47a3d1da3a190b55c7bccda9575f44c9169893d
a-kenji <aks.kenji@protonmail.com>
2022-08-30 21:28:14 +0200

    mob next [ci-skip] [ci skip] [skip ci]
    lastFile:crates/control2json/src/main.rs

--- crates/control2json/src/main.rs
+++ crates/control2json/src/main.rs
@@ -3,7 +3,26 @@ use std::{
     io::{self, Read, Write},
 };
 
-fn main() -> std::io::Result<()> {
+use control_file::ControlFile;
+
+use self::error::Control2JsonError;
+
+mod error {
+    use thiserror::Error;
+    /// The debnix error type
+    #[derive(Error, Debug)]
+    pub enum Control2JsonError {
+        /// Io Error
+        #[error("IoError: {0}")]
+        Io(#[from] std::io::Error),
+        #[error("Utf8 Conversion Error")]
+        Utf8(#[from] std::str::Utf8Error),
+        #[error("Control File Error {0}")]
+        ControlFile(#[from] control_file::ControlFileError),
+    }
+}
+
+fn main() -> Result<(), Control2JsonError> {
     let args: Vec<String> = env::args().collect();
     let input = &args[1];
 
@@ -11,8 +30,10 @@ fn main() -> std::io::Result<()> {
         let mut buffer = Vec::new();
         let mut reader = io::stdin().lock();
         reader.read_to_end(&mut buffer)?;
+        let pkgs = pkgs_from_control_file(std::str::from_utf8(&buffer)?);
         let mut stdout = io::stdout();
-        stdout.write_all(&buffer)?;
+        let fmt = format!("{:?}", pkgs);
+        stdout.write_all(fmt.as_bytes())?;
     } else {
         let mut buffer = Vec::new();
         let mut reader = fs::File::open(input).unwrap();
@@ -22,4 +43,9 @@ fn main() -> std::io::Result<()> {
     }
 
     Ok(())
+}
+
+fn pkgs_from_control_file(control_file: &str) -> Result<Vec<String>, Control2JsonError> {
+    let control_file = ControlFile::from_str(control_file)?;
+    Ok(control_file.get_dependencies()?)
 }
