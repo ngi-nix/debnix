a37b2dbbbd8b93dbd79dd959ff6df900152cfb77
a-kenji <aks.kenji@protonmail.com>
2022-08-30 20:48:26 +0200

    mob next [ci-skip] [ci skip] [skip ci]
    lastFile:crates/control2json/src/main.rs

--- crates/control2json/src/main.rs
+++ crates/control2json/src/main.rs
@@ -1,3 +1,36 @@
-fn main() {
-    println!("Hello, world!");
+use std::{
+    env, fs,
+    io::{self, stdout, BufRead, Write},
+};
+
+fn main() -> std::io::Result<()> {
+    let args: Vec<String> = env::args().collect();
+    let input = &args[1];
+
+    if input == "-" {
+        let mut output = String::new();
+        let mut reader = io::stdin().lock();
+        for line in reader.lines() {
+            println!("{}", line?);
+        }
+    } else {
+        let mut reader = fs::File::open(input).unwrap();
+    }
+
+    // let mut input = String::new();
+    // match io::stdin().read_line(&mut input) {
+    // for line in reader.lines() {
+    //     println!("{}", line.unwrap());
+    // }
+
+    // let mut buffer = Vec::new();
+    // // reader.read_line(&mut buffer)?;
+    // let mut output = String::new();
+    // reader.read_to_string(&mut output)?;
+    // println!("{}", output);
+    //
+    // let mut stdout = io::stdout();
+    // stdout.write_all(&buffer)?;
+
+    Ok(())
 }
